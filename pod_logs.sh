d3b9eb60569c980a573115700aae8f88a5f9ebd3
Parveen Chahal <mail.chahal@gmail.com>
2021-12-07 02:04:16 +0530

    Create pod_logs.sh

---
+++ pod_logs.sh
@@ -0,0 +1,24 @@
+pat=$1
+if [ -z $pat ]
+then
+  echo 'Filter pattern not provided'
+  exit
+fi
+
+t=$2
+if [ -z $t ]
+then
+  t=0
+fi
+
+pat_res=$(kubectl get pod -A | grep -i $pat | wc -l)
+if [ $pat_res -gt 1 ]
+then
+  echo 'Multiple results for pattern $pat'
+  exit
+fi
+
+ns=$(kubectl get pod -A | grep -i $pat | awk '{print $1}')
+pod=$(kubectl get pod -A | grep -i $pat | awk '{print $2}')
+
+kubectl logs $pod -n $ns --tail=$t -f
